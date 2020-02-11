affdd5d005851eef9ddbb8ecca5814ecf7032c7c
joy2362 <abdullahzahidjoy@gmail.com>
2020-02-11 21:13:39 +0600

    initial commit

--- index.php
+++ index.php
@@ -704,7 +704,6 @@
             </div>
         </div>
     </div>
-    
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
     <script src="js/swiper.min.js"></script>
